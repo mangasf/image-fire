914eb10669a10e9fe43e307da4cf8201bac72fbf
Francisco Mangas <francisco.mangas.79@gmail.com>
2018-05-19 18:18:26 +0200

    Update image model.

--- share/www/html/image-fire/db/images.sql
+++ share/www/html/image-fire/db/images.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS Images (
   id      INT(11)      NOT NULL AUTO_INCREMENT,
   name    VARCHAR(100) NOT NULL,
   contain VARCHAR(100) NOT NULL,
+  description VARCHAR(256),
+  tags VARCHAR(256),
   PRIMARY KEY (id)
 )
